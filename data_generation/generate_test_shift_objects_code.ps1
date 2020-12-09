52d401d95e977cf75abd86101b1f515c39c52143
Thomas Orton <thorton@fastmail.com>
2020-12-09 03:05:13 +0000

    Add Person data generation

--- data_generation/generate_test_shift_objects_code.ps1
+++ data_generation/generate_test_shift_objects_code.ps1
@@ -1,4 +1,12 @@
-Param([int] $number_to_generate)
+Param([ValidateSet("Shift", "Person")] $type, [int] $number_to_generate)
+
+Enum TypeToGenerate
+{
+    Shift
+    Person
+}
+
+$typeToGenerate = [TypeToGenerate]$type
 
 $r = New-Object System.Random
 
@@ -9,31 +17,60 @@ $first_names_count = 4945
 $last_names_count = 21985
 $plays_and_musicals = 342
 
-$objCode = ""
+function generateShifts()
+{
+    $objCode = ""
 
-foreach ($i in 1..$number_to_generate) {
-    $yr = $r.Next(2021, 2022)
-    $mon = $r.Next(1, 12)
-    $day = $r.Next(1, 28)
-    $time_range = $time_ranges[$r.Next(1, $time_ranges_max_ix)]
+    foreach ($i in 1..$number_to_generate) {
+        $yr = $r.Next(2021, 2022)
+        $mon = $r.Next(1, 12)
+        $day = $r.Next(1, 28)
+        $time_range = $time_ranges[$r.Next(1, $time_ranges_max_ix)]
+        
+        $name = getRandomEntry "plays_and_musicals.txt" $plays_and_musicals
 
-    #$start = New-Object System.DateTime 
-    #$end = New-Object System.DateTime $yr, $mon, $day, $time_range[2], $time_range[3]
-    
-    $name = getRandomEntry "plays_and_musicals.txt" 342
+        $objCode += "        {
+                Name = ""$name""
+                Start = new DateTime($yr, $mon, $day, $($time_range[0]), $($time_range[1]), 00)
+                End = new DateTime($yr, $mon, $day, $($time_range[2]), $($time_range[3]), 00)
+                Staff = Array.empty
+            }`n"
+    }
 
-    $objCode += "        {
-            Name = ""$name""
-            Start = new DateTime($yr, $mon, $day, $($time_range[0]), $($time_range[1]), 00)
-            End = new DateTime($yr, $mon, $day, $($time_range[2]), $($time_range[3]), 00)
-            Staff = Array.empty
-        }`n"
+    $objCode
 }
 
-$objCode
+function generatePersons()
+{
+    $objCode = ""
 
-function getRandomEntry($fileName, $totalEntriesCount) {
+    foreach ($i in 1..$number_to_generate) {
+        $first_name = getRandomEntry "first-names.txt" $first_names_count
+        $last_name = getRandomEntry "last-names.txt" $last_names_count
+
+        $objCode += "        {
+            Forename = ""$first_name""
+            Surname = ""$last_name""
+            UnavailableDates = Array.empty
+            Shifts = Array.empty
+        }`n"
+    }
+
+    $objCode
+}
+
+function getRandomEntry($fileName, $totalEntriesCount)
+{
     $tc = $r.Next(1, $totalEntriesCount)
     $entries = Get-Content $fileName -TotalCount $tc
     return $entries[$tc - 1]
+}
+
+if ($typeToGenerate -eq [TypeToGenerate]::Shift)
+{
+    generateShifts
+}
+else
+{
+    generatePersons
 }
