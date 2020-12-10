e425429ae1b8c93faab89756f1e030d6c18e55dd
Thomas Orton <thorton@fastmail.com>
2020-12-10 23:18:06 +0000

    Complete basic working implementation of invert shifts function
    However some tests fail because of non-matching order of shifts.

--- src/Scheduler.fs
+++ src/Scheduler.fs
@@ -12,5 +12,23 @@ module Scheduler =
     let invertShifts(shifts: Shift[]) =
         let staff = Set.empty<Person>
         shifts
-        |> Array.collect (fun s -> s.Staff)
-        |> Array.fold (fun ss person -> if (Set.contains person ss) then ss else (Set.add person ss)) staff
+        |> Array.fold
+            (fun staff' shift -> shift.Staff |> Array.fold
+                                    (fun staff'' staffMember ->
+                                        let matchingStaff = staff''
+                                                            |> Set.filter (fun sm -> {sm with Shifts = Array.empty} = staffMember)
+                                        let stafflessShift = { shift with Staff = Array.empty }
+                                        if (Set.isEmpty matchingStaff) then
+                                            Set.add {staffMember with Shifts = [| stafflessShift |]} staff''
+                                        else
+                                            // TODO: modify the matching staff member (in matchingStaff) that we have
+                                            // just found instead of iterating through staff'' again
+                                            staff''
+                                            |> Set.map
+                                                (fun sm ->
+                                                    if {sm with Shifts = Array.empty} = staffMember then
+                                                        { sm with Shifts = Array.ofList (stafflessShift :: (List.ofArray sm.Shifts))  }
+                                                    else
+                                                        sm))
+                                    staff')
+                                staff
