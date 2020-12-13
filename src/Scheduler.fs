8510940a2c31dc410141b29293c6cb0010ebdae4
Thomas Orton <thorton@fastmail.com>
2020-12-13 03:59:40 +0000

    Re-implement invert shifts func: _much_ shorter & more elegant

--- src/Scheduler.fs
+++ src/Scheduler.fs
@@ -17,25 +17,8 @@ module Scheduler =
         decimal shiftStaff.Length / decimal uniqueStaff.Length
 
     let invertShifts(shifts: Shift[]) =
-        let staff = Set.empty<Person>
         shifts
-        |> Array.fold
-            (fun staff' shift -> shift.Staff |> Array.fold
-                                    (fun staff'' staffMember ->
-                                        let matchingStaff = staff''
-                                                            |> Set.filter (fun sm -> {sm with Shifts = Set.empty} = staffMember)
-                                        let stafflessShift = { shift with Staff = Array.empty }
-                                        if (Set.isEmpty matchingStaff) then
-                                            Set.add {staffMember with Shifts = Set.singleton stafflessShift } staff''
-                                        else
-                                            // TODO: modify the matching staff member (in matchingStaff) that we have
-                                            // just found instead of iterating through staff'' again
-                                            staff''
-                                            |> Set.map
-                                                (fun sm ->
-                                                    if {sm with Shifts = Set.empty} = staffMember then
-                                                        { sm with Shifts = Set.ofList (stafflessShift :: (Set.toList sm.Shifts))  }
-                                                    else
-                                                        sm))
-                                    staff')
-                                staff
+        |> Array.collect (fun s -> s.Staff |> Array.map (fun sm -> sm, { s with Staff = Array.empty }))
+        |> Array.groupBy fst
+        |> Array.map (fun g -> { fst g with Shifts = snd g |> Array.map (fun ss -> snd ss) |> Set.ofArray })
+        |> Set.ofArray
