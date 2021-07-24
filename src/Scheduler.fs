b65c98009672afcb2288377b52a8279f60ddb779
Thomas Orton <thorton@fastmail.com>
2021-07-24 21:24:56 +0100

    Implement most rudimentary simulated annealing shift calculation

--- src/Scheduler.fs
+++ src/Scheduler.fs
@@ -2,6 +2,7 @@ namespace Scheduling
 
 module Scheduler =
 
+    open System
     open Types
 
     let calculateMeanShiftsPerStaffMember shifts =
@@ -34,6 +35,10 @@ module Scheduler =
     let calculateCost shifts =
         calculateVariance shifts
 
+    // TODO: do we need this? Surely if hard constraints are not met these should just be flagged to
+    // the user afterwards (so they can find a solution later) rather than refusing to calculate a
+    // schedule because, for example, there are not enough available staff for 1 shift.
+    // Or are there constraints that should stop us in our tracks?
     let meetsHardConstraints shifts staff =
         // TODO: implement this properly with checks for different kinds of failure to meet requirements
         if false then
@@ -64,4 +69,57 @@ module Scheduler =
         | Error requirement ->
             Error requirement
         | Ok (shifts, staff) ->
-            calculateInitialSchedule shifts staff
+            match calculateInitialSchedule shifts staff with
+            | Error err ->
+                Error err
+            | Ok initialSchedule ->
+                let mutable temp = 1500.0
+                let alpha = 0.9
+                let numOfTempReductions = 250
+                let numOfNeighboursToSearch = 20
+
+                let rand = System.Random();
+
+                let shiftCount = Array.length shifts
+
+                let getDifferentRandomNumbers exclMax =
+                    let first = rand.Next(0, exclMax)
+                    let mutable second = first
+                    while second = first do
+                        second <- rand.Next(0, exclMax)
+                    first, second
+
+                // move operator
+                let move (shifts: Shift[]) =
+                    // TODO: can we swap two random shift staff members more elegantly?
+                    let shift1Ix, shift2Ix = getDifferentRandomNumbers shiftCount
+                    let shift1 = shifts.[shift1Ix]
+                    let shift2 = shifts.[shift2Ix]
+                    let shift1StaffIx = rand.Next(0, (Array.length shift1.Staff))
+                    let shift2StaffIx = rand.Next(0, (Array.length shift2.Staff))
+                    let temp = shift2.Staff.[shift2StaffIx]
+                    shift2.Staff.[shift2StaffIx] <- shift1.Staff.[shift1StaffIx]
+                    shift1.Staff.[shift1StaffIx] <- temp
+                    shifts
+
+                let mutable currSchedule = initialSchedule
+
+                for i in 0..numOfNeighboursToSearch do
+                    for j in 0..numOfNeighboursToSearch do
+                        let currCost = calculateCost currSchedule
+
+                        let newSchedule = move currSchedule
+                        let newCost = calculateCost newSchedule
+
+                        if newCost <= currCost then
+                            currSchedule <- newSchedule
+                        else
+                            let r = rand.Next(0, 100) |> float |> (/) <| 100.0
+
+                            let value = 1.0 / Math.Exp((newCost - currCost) / temp)
+
+                            if r <= value then
+                                currSchedule <- newSchedule
+
+                        temp <- temp * alpha
+                Ok currSchedule
--- tests/CalculateScheduleTests.fs
+++ tests/CalculateScheduleTests.fs
@@ -7,32 +7,4 @@ open Scheduling.Types
 open Common
 open System
 
-[<Fact>]
-let ``outputs same as input``() =
-    let shifts = [|
-        assassins
-        dixie
-        priscilla
-    |]
-
-    let staff: StaffMember[] = [||]
-    
-    test <@ (calculateSchedule shifts staff) = Ok shifts @>
-
-[<Fact>]
-let ``returns error if feasible schedule is impossible`` =
-    let shifts = [|
-        assassins
-        dixie
-        priscilla
-    |]
-
-    let staff = [|
-        { nancey with UnavailableDates = [|
-                    {
-                        Start = new DateTime(2021, 3, 1)
-                        End = new DateTime(2021, 3, 6)
-                    }
-        |] } 
-    |]
-    ()
+// TODO
