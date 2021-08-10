e698be74cd455935624feb3235df6519d799fb19
Thomas Orton <thorton@fastmail.com>
2021-08-11 00:29:12 +0100

    Add module for simulated annealing-based calculation

---
+++ src/SimulatedAnnealingScheduler.fs
@@ -0,0 +1,26 @@
+namespace Scheduling
+
+module SimulatedAnnealingScheduler =
+
+    open System
+    open Types
+
+    let calculateInitialSchedule shifts staff =
+        failwith "Not implemented"
+
+    let calculateCost shifts =
+        failwith "Not implemented"
+
+    let moveToNeighbour shifts staff =
+        failwith "Not implemented"
+
+    let satisfiesHardConstraints shifts =
+        failwith "Not implemented"
+
+    // if solution fails to satisfy hard constraints we apply a penalty to the cost
+    let applyPenalty cost =
+        // TODO
+        failwith "Not implemented"
+
+    let calculateSchedule shifts staff =
+        failwith "Not implemented"
