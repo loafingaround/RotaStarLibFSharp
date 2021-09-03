a59b987e8ac1c9954d43d3fb9c2b042f0edfa2bc
Thomas Orton <thorton@fastmail.com>
2021-09-03 19:32:01 +0100

    Make script reference paths relative

--- shift_display.fsx
+++ shift_display.fsx
@@ -1,7 +1,7 @@
 #I "XPlot.GoogleCharts/XPlot.GoogleCharts.3.0.1/lib/netstandard2.0"
 #r "XPlot.GoogleCharts.dll"
-#r "/general/Development/Projects/Rotaring/RotaStarLibFSharp/src/bin/Debug/netstandard2.0/Scheduling.dll"
-#r "/general/Development/Projects/Rotaring/RotaStarLibFSharp/tests/bin/Debug/netcoreapp3.1/Scheduling.Tests.dll"
+#r "src/bin/Debug/netstandard2.0/Scheduling.dll"
+#r "tests/bin/Debug/netcoreapp3.1/Scheduling.Tests.dll"
 
 open XPlot.GoogleCharts
 open Scheduling.Types
