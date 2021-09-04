ca2367ab6d5ba457c99df92930701d110e5bf5cc
Thomas Orton <thorton@fastmail.com>
2021-09-04 16:00:24 +0100

    WIP: Add (mostly) failing test for duplicate staff on shift
    Needs "random" numbers function which produces same infinite sequence
    each time, so test always fails and always in the same way.

--- tests/SimulatedAnnealingScheduler/CalculateScheduleTests.fs
+++ tests/SimulatedAnnealingScheduler/CalculateScheduleTests.fs
@@ -1,10 +1,12 @@
 module CalculateScheduleTests
 
+open System
 open Xunit
 open Swensen.Unquote
 open Scheduling.SimulatedAnnealingScheduler
 open Scheduling.Types
 open Common
+open Utilities
 
 let nextDummyRandom _ =
     0
@@ -14,8 +16,6 @@ let ``Returns appropriate error for 0 shifts``() =
     let shifts = Array.empty<Shift>
 
     let staff = [| nancey |]
-
-    let expected: Shift[] = Array.empty
 
     test <@ calculateSchedule nextDummyRandom shifts staff = Error NoShifts @>
 
@@ -53,3 +53,46 @@ let ``Returns Ok result given 1 shift and 1 staff member``() =
             Assert.True(false, "No shifts")
         | NoStaff ->
             Assert.True(false, "No staff")
+
+[<Fact>]
+let ``Does not return shifts with same staff more than once on shift``() =
+    let staff = [| nancey; britte; cheryl |]
+
+    let shifts = [|
+            { assassins with MaximumNumberOfStaff = 2 };
+            { dixie  with MaximumNumberOfStaff = 3 };
+            { priscilla with MaximumNumberOfStaff = 1 }
+        |]
+
+    // TODO: substitute nextRandom for function producing infinite repeating sequence
+    // e.g. see https://stackoverflow.com/questions/8919006/infinite-sequence-with-repeating-elements
+    match calculateSchedule nextRandom shifts staff with
+    | Ok schedule ->
+        let withDuplicates =
+            schedule
+            |> Array.map (fun s ->
+                let counts =
+                    s.Staff
+                    |> Array.countBy (fun sm -> sm.Id)
+                    |> Array.where (fun c -> snd c > 1)
+                s, counts)
+            |> Array.where (fun sc -> Array.length (snd sc) > 0)
+        let getStaffMemberById id =
+            staff |> Array.find (fun s -> s.Id = id)
+        let getStaffMemberNames idsAndCounts =
+            let names =
+                idsAndCounts
+                |> Array.map (fun ic ->
+                    match ic with
+                    | id, count ->
+                        let staffMember = getStaffMemberById id
+                        sprintf "\t%s %s: %i times" staffMember.Forename staffMember.Surname count)
+            String.Join("\n", names)
+        let getMsg (withDuplicates: (Shift * (int * int)[])[]) =
+            let names =
+                withDuplicates
+                |> Array.map (fun wd -> sprintf "%s\n%s" (fst wd).Name (getStaffMemberNames (snd wd)))
+            "These shifts have the same staff multiple times:\n" + String.Join("\n", names)
+        Assert.True(Array.isEmpty withDuplicates, getMsg withDuplicates)
+    | Error _ ->
+        ()
